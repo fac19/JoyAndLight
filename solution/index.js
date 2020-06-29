9f37e1e604508501fb50a133e160a2c3133e0150
Jack Heriz-Smith <jackherizsmith@gmail.com>
2020-06-29 14:18:52 +0100

    remove solution

--- solution/index.js
+++
@@ -1,6 +0,0 @@
-const server = require("./server.js");
-const PORT = process.env.PORT || 3333;
-
-const listener = server.listen(PORT, () =>
-  console.log(`Listening on http://localhost:${listener.address().port}`)
-);
